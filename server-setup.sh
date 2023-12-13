6a6f9d175036047e7143bcf11da88e6eb822c852
JB <fraissejb@gmail.com>
2023-12-13 19:52:46 +0100

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+export LANG="FR"
+export STATE="France"
+export LOCALITY="Paris"
+export ORGANISM=""
+export UNIT=""
+export NAME=""
+export MAIL=""
+
+echo -n Choose a password: 
+read -s pass
+
 ## Creating and moving to a specific directory to create the certificates
 mkdir config-files
 cd config-files
 
 ## Creating certification authority
 openssl genrsa -des3 -out ca.key 4096
-printf '\n\n\n\n\n\n\n' | openssl req -new -x509 -utf8 -days 36500 -key ca.key -out ca.crt 
+printf '$LANG\n$STATE\n$LOCALITY\n$ORGANISM\n$UNIT\n$NAME\n$MAIL\n\n' | openssl req -new -x509 -utf8 -days 36500 -key ca.key -out ca.crt
+echo -n
 
 ## Creating server's certification
 cat <<EOF > openssl.x509.server.conf 
@@ -16,7 +28,7 @@ extendedKeyUsage=serverAuth
 EOF
 
 openssl genrsa -out server.key 4096
-printf '\n\n\n\n\n\n\n' | openssl req -new -utf8 -key server.key -out server.csr
+printf '$LANG\n$STATE\n$LOCALITY\n$ORGANISM\n$UNIT\n$NAME\n$MAIL\n\n' | openssl req -new -utf8 -key server.key -out server.csr
 openssl x509 -req -days 36500 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt -extfile openssl.x509.server.conf
 rm server.csr
 
