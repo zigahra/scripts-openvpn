01d904d5c5501215c0c1c9c1c8f1d2cc217335a7
JB <fraissejb@gmail.com>
2023-12-13 19:21:37 +0100

    Create server-setup.sh

---
+++ server-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Creating certification authority
+openssl genrsa -des3 -out ca.key 4096
+openssl req -new -x509 -utf8 -days 36500 -key ca.key -out ca.crt
+
+## Creating server's certification
+cat <<EOF > openssl.x509.server.conf 
+# These extensions are added when 'ca' signs a request.
+keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage=serverAuth
+EOF
+
+openssl genrsa -out server.key 4096
+openssl req -new -utf8 -key server.key -out server.csr
+openssl x509 -req -days 36500 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt -extfile openssl.x509.server.conf
+rm server.csr
+
+## Creating the tls-auth key
+openvpn --genkey --secret ta.key
+
+## Creating Diffie-Hellman file
+#openssl dhparam -out dh4096.pem 4096
